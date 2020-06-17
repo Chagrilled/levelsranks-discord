6ad38971fc5313227d9524ca78e1d009780ab298
Chagrilled <Andrew.n64@hotmail.com>
2020-06-17 21:56:35 +0100

    Steam API support for grabbing profile image and link

--- bot.js
+++ bot.js
@@ -2,8 +2,9 @@ const Discord = new require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 const mysql = require('mysql2');
+const steamAPI = new (require('steamapi'))(config.steamAPIKey)
+const steamID = require('steamid');
 const PREFIX = config.commandPrefix;
-
 client.once('ready', () => console.log("I'm a bot!"));
 
 client.login(config.discordKey);
@@ -34,12 +35,15 @@ const Top = message => {
     });
 };
 
-const Rank = message => {
+const Rank = async message => {
     if (message.content == `${PREFIX}rank`)
         return message.reply(`Displays stats for a user's Steam ID:\n**Usage:** ${PREFIX}rank STEAM_1:0:12345`);
 
     const steam = message.content.match(/rank (STEAM_[0-9]:[0-9]:[0-9]+)/);
     if (!steam || !steam[1]) return message.reply(`Invalid Steam ID given\n**Usage:** ${PREFIX}rank STEAM_1:0:1234`);
+    let sid = new steamID(steam[1]);
+
+    profile = await steamAPI.getUserSummary(sid.getSteamID64());
 
     pool.getConnection((err, con) => {
         con.query(`SELECT name, value, rank, kills, deaths, headshots, hits, assists, round_win, round_lose, playtime FROM lvl_base WHERE steam = '${steam[1]}'`, (err, result) => {
@@ -52,6 +56,8 @@ const Rank = message => {
             let embed = new Discord.MessageEmbed()
                 .setColor("40C0EF")
                 .setTitle(`${result[0].name}'s Profile`)
+                .setThumbnail(profile ? profile.avatar.medium : "")
+                .setURL(profile ? profile.url : "")
                 .addFields(
                     { name: "Kills",         value: result[0].kills, inline: true },
                     { name: "Deaths",        value: result[0].deaths, inline: true },
@@ -60,14 +66,14 @@ const Rank = message => {
                     { name: "KDR",           value: (result[0].kills / (result[0].deaths || 1)).toFixed(2), inline: true },
                     { name: "HS Percentage", value: (result[0].headshots / (result[0].kills || 1) * 100).toFixed(2), inline: true },
                     { name: "Rounds Played", value: result[0].round_win + result[0].round_lose, inline: true }
-                );
+                )
             con.release();
             return message.reply(embed);
         });
-    })
+    });
 };
 
-client.on('message', message => {
+client.on('message', async message => {
     if (message.author === client.user) return;
 
     if (message.content.startsWith(`${PREFIX}top`)) return Top(message);
--- readme.md
+++ readme.md
@@ -5,6 +5,7 @@ Discord bot for use with [LevelsRanks](https://github.com/levelsranks/levels-ran
 ## Install
 - Clone this repo to your host machine that has npm/node installed
 - Create your bot application in Discord
+- Get a Steam API key from [here](https://steamcommunity.com/dev/apikey)
 - Fill in the `config.json` with your database details and bot key
 - On your host machine, go to the repo you cloned into:
 ```bash
